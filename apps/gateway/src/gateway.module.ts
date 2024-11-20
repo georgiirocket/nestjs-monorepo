1f9a6f1029acd8dfdf3ffa581dbb176992bf11cd
g_rocket <georgiiromanishko@gmail.com>
2024-11-20 19:50:01 +0100

    feat(project): gateway service

--- apps/gateway/src/gateway.module.ts
+++ apps/gateway/src/gateway.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
+import { ConfigModule } from '@nestjs/config';
+import { ENV_FILE_PATH } from '@app/libs/constants';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+      validationSchema: Joi.object({
+        GATEWAY_PORT: Joi.string().required(),
+      }),
+    }),
+  ],
   controllers: [GatewayController],
   providers: [GatewayService],
 })
